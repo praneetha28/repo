b6119b63b62b4265e8d5c23a2d5469eda34b5f8a
praneetha <praneetha@LAPTOP-8KL032v4>
2018-08-01 18:27:17 +0530

    compare commit

---
+++ cspp1-practisem3/compare_AB.py
@@ -0,0 +1,12 @@
+varA = "hello"
+varB = "hi"
+if type(varA)==str or type(varB)==str:
+    print (" string involved")
+elif varA>varB:
+  print (" bigger ")
+elif varA==varB:
+  print ( "equal" )
+else:
+  print(" smaller ")
+
+   
