b351789017da3fd3d381bba3b04935a9b9eb03c8
praneetha <praneetha@LAPTOP-8KL032v4>
2018-08-02 16:52:29 +0530

    folder

---
+++ cspp1-practisem3/sample-assignment/sol.py
@@ -0,0 +1,12 @@
+# Exercise: Assignment-1
+# Write a Python function, factorial(n), that takes in one number and
+# returns the factorial of given number.
+
+# This function takes in one number and returns one number.
+
+def main():
+    data = input()
+    print(int(data[0])+int(data[1]))
+
+if __name__ == "__main__":
+    main()
