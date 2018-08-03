b2821400eb5e66b0e67fa42b48acb3987dd44cdb
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-03 16:27:54 +0530

    folder

---
+++ cspp1-practisem3/m5/p1/perfect_cube.py
@@ -0,0 +1,19 @@
+# Write a python program to find if the given number is a perfect cube or not
+# using guess and check algorithm
+
+# testcase 1
+# Input: 24389
+# Output: 24389 is a perfect cube
+
+# testcase 2
+# Input: 21950
+# Output: 21950 is not a perfect cube
+
+def main():
+	# input is captured in s
+	s = raw_input()
+	# watch out for the data type of value stored in s
+	# your code starts here
+
+if __name__== "__main__":
+	main()
