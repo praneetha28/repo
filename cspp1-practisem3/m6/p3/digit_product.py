48667999ce6f5344c4c4476a5256fa42e30ae340
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-04 11:44:34 +0530

    " -> 5 of 7 passed. pylint: 10.00/10 "

--- cspp1-practisem3/m6/p3/digit_product.py
+++ cspp1-practisem3/m6/p3/digit_product.py
@@ -1,8 +1,12 @@
 "'# enter input'"
 N = int(input())
 SUM = 1
-while abs(N) > 0:
-    SUM = SUM * (abs(N) % 10)
-    N = int(abs(N)/10)
-SUM = -SUM
-print(SUM)
+
+if N == 0:
+    print(N)
+else:
+    while abs(N) > 0:
+        SUM = SUM * (abs(N) % 10)
+        N = int(abs(N)/10)
+        SUM = -SUM
+    print(SUM)
