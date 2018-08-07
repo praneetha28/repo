189874ced8e26c74a7dea627967a76b0ec94f050
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-07 22:40:50 +0530

    " -> 8 of 10 passed. pylint: 10.00/10 "

--- cspp1-practisem3/m7/p3/Functions - Assignment-3/assignment3.py
+++ cspp1-practisem3/m7/p3/Functions - Assignment-3/assignment3.py
@@ -1,14 +1,41 @@
+"'#input'"
+def paying_debt_offina_year(balance, annual_interest_rate):
+    '''
+    function
+    '''
+    monthly_interest_rate = (annual_interest_rate) / 12.0
+    monthly_payment_lower_bound = balance / 12
+    monthly_payment_upper_bound = (balance * (1 + monthly_interest_rate)**12) / 12.0
+    new_balance = balance
+    increment = 0.0001
+    guess = (monthly_payment_lower_bound + monthly_payment_upper_bound)/2
+    while True:
+        month = 1
+        while month <= 12:
+            new_balance = new_balance - guess
+            new_balance = new_balance + (monthly_interest_rate * new_balance)
+            month += 1
+        if new_balance > 0 and new_balance > increment:
+            monthly_payment_lower_bound = guess
+            new_balance = balance
+        elif new_balance < 0 and new_balance < -increment:
+            monthly_payment_upper_bound = guess
+            new_balance = balance
+        else:
+            return guess
 
-
-def payingDebtOffInAYear(balance, annualInterestRate):
+        guess = (monthly_payment_lower_bound + monthly_payment_upper_bound)/2
 
 
 def main():
+    '''
+    call
+    '''
     data = input()
     # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
-    print(payingDebtOffInAYear(data[0],data[1]))
-    
-if __name__== "__main__":
+    print("Lowest Payment: "+str(round(paying_debt_offina_year(data[0], data[1]), 2)))
+
+if __name__ == "__main__":
     main()
