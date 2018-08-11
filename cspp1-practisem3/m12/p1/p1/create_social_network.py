ba0565e5b17476d7a203d8a4d5fcb2cf47a9f8a4
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-11 13:07:47 +0530

    " -> 0 of 2 passed. pylint: 9.68/10 "

---
+++ cspp1-practisem3/m12/p1/p1/create_social_network.py
@@ -0,0 +1,56 @@
+'''
+    Assignment-1 Create Social Network
+'''
+
+def create_social_network(data):
+    '''
+        The data argument passed to the function is a string
+        It represents simple social network data
+        In this social network data there are people following other people
+
+        Here is an example social network data string:
+        John follows Bryant,Debra,Walter
+        Bryant follows Olive,Ollie,Freda,Mercedes
+        Mercedes follows Walter,Robin,Bryant
+        Olive follows John,Ollie
+
+        The string has multiple lines and each line represents one person
+        The first word of each line is the name of the person
+        The second word is follows that separates the person from the followers
+        After the second word is a list of people separated by ,
+
+        create_social_network function should split the string on lines
+        then extract the person and the followers by splitting each line
+        finally add the person and the followers to a dictionary and
+        return the dictionary
+
+        Caution: watch out for trailing spaces while splitting the string.
+        It may cause your test cases to fail although your output may be right
+
+        Error handling case:
+        Return a empty dictionary if the string format of the data is invalid
+        Empty dictionary is not None, it is a dictionary with no keys
+    '''
+    string_dict={}
+    for i in data:
+        import re
+        res=re.split('follows |\n',data)
+        for k,v in res:
+            string_dict={res[0]:values(res[0])}
+    return string_dict
+
+def main():
+    '''
+        handling testcase input and printing output
+    '''
+    string = ''
+    lines = int(input())
+    for i in range(lines):
+        i += 1
+        string += input()
+        string += '\n'
+
+    print(create_social_network(string))
+
+if __name__ == "__main__":
+    main()
---
+++ cspp1-practisem3/m12/p2/p2/social_network.py
@@ -0,0 +1,70 @@
+'''
+    This is a continuation of the social network problem
+    There are 3 functions below that have to be completed
+    Note: PyLint score need not be 10/10 for this assignment. We expect 9.5/10
+'''
+
+def follow(network, arg1, arg2):
+    '''
+        3 arguments are passed to this function
+        network is a dictionary representing the social network
+        arg1 and arg2 are two people in the network
+        follow function is called when arg1 wants to follows arg2
+        so, this should result in adding arg2 to the followers list of arg1
+        update the network dictionary and return it
+    '''
+    if arg1 and arg2 in network:
+        network[arg1].append(arg2)
+    network.update()
+    return network
+
+def unfollow(network, arg1, arg2):
+    '''
+        3 arguments are passed to this function
+        network is a dictionary representing the social network
+        arg1 and arg2 are two people in the network
+        unfollow function is called when arg1 wants to stop following arg2
+        so, this should result in removing arg2 from the followers list of arg1
+        update the network dictionary and return it
+    '''
+    if arg1 and arg2 in network:
+        network[arg1].remove(arg2)
+    network.update()
+    return network
+
+def delete_person(network, arg1):
+    '''
+        2 arguments are passed to this function
+        network is a dictionary representing the social network
+        arg1 is a person in the network
+        delete_person function is called when arg1 wants to exit from the network
+        so, this should result in deleting arg1 from network
+        also, before deleting arg1, remove arg1 from the everyone's followers list
+        update the network dictionary and return it
+    '''
+    if arg1 in network:
+        del network[arg1]
+    network.update()
+    return network
+
+def main():
+    '''
+        handling testcase input and printing output
+    '''
+    network = eval(input())
+    lines = int(input())
+    for i in range(lines):
+        i += 1
+        line = input()
+        output = line.split(" ")
+        if output[0] == "follow":
+            network = follow(network, output[1], output[2])
+        elif output[0] == "unfollow":
+            network = unfollow(network, output[1], output[2])
+        elif output[0] == "delete":
+            network = delete_person(network, output[1])
+
+    print(network)
+
+if __name__ == "__main__":
+    main()
