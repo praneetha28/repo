305047b645cb9e33c1f41afcd57b3d278671a962
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-23 16:35:55 +0530

    " -> 7 of 8 passed. pylint: 7.89/10 "

--- cspp1-practisem3/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-practisem3/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -1,22 +1,22 @@
 '''program'''
-def mult_matrix(mat_m1, mat_m2):
+def mult_matrix(m1, m2):
     ''' check if the matrix1 columns = matrix2 rows
         mult the matrices and return the result matrix
         print an error message if the matrix shapes are not valid for mult
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    if len(mat_m1[0]) == len(mat_m2) and len(mat_m1[0]) == len(mat_m2):
-        res = [[0 for row in range(len(mat_m1))] for col in range(len(mat_m2[1]))]
-        for i in enumerate(mat_m1):
-            for j in range(len(mat_m2[1])):
-                for k in enumerate(mat_m2):
-                    res[i][j] += mat_m1[i][k]*mat_m2[k][j]
+    if len(m1[0]) == len(m2) and len(m1[0]) == len(m2):
+        res = [[0 for row in range(len(m1))] for col in range(len(m2[1]))]
+        for i in range(len(m1)):
+            for j in range(len(m2[1])):
+                for k in range(len(m2)):
+                    res[i][j] += m1[i][k]*m2[k][j]
         return res
     print("Error: Matrix shapes invalid for mult")
     return None
 
-def add_matrix(mat_m1, mat_m2):
+def add_matrix(m1, m2):
     '''
         check if the matrix shapes are similar
         add the matrices and return the result matrix
@@ -24,11 +24,11 @@ def add_matrix(mat_m1, mat_m2):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    if len(mat_m1) == len(mat_m2) and len(mat_m1[0]) == len(mat_m2[0]):
-        res = [[0 for row in range(len(mat_m1[0]))] for col in range(len(mat_m1))]
-        for i in enumerate(mat_m1):
-            for j in range(len(mat_m1[0])):
-                res[i][j] = mat_m1[i][j] + mat_m2[i][j]
+    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
+        res = [[0 for row in range(len(m1[0]))] for col in range(len(m1))]
+        for i in range(len(m1)):
+            for j in range(len(m1[0])):
+                res[i][j] = m1[i][j] + m2[i][j]
         return res
     print("Error: Matrix shapes invalid for addition")
     return None
