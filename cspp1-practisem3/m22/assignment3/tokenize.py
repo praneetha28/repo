ded57c2872a69257aa4896b61a06cd6fa7b4600f
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-25 11:54:22 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

---
+++ cspp1-practisem3/m22/assignment3/tokenize.py
@@ -0,0 +1,26 @@
+'''
+Write a function to tokenize a given string and return a dictionary with the frequency of
+each word
+'''
+import re
+def tokenize(string):
+    '''token function'''
+    input1 = re.split(r'[^a-zA-Z]', string.strip())
+    while '' in input1:
+        input1.remove('')
+    dict1 = {}
+    for word in input1:
+        dict1[word] = input1.count(word)
+    return dict1
+
+def main():
+    '''main function'''
+    num = int(input())
+    input1 = ""
+    for _ in range(num):
+        input1 += input()
+    print(input1)
+    print(tokenize(input1))
+
+if __name__ == '__main__':
+    main()
