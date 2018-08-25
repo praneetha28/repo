dca9fa64a45197b7de89473e01ca258d49f71f90
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-25 15:42:59 +0530

    " -> 14 of 14 passed. pylint: -2.00/10 "

--- cspp1-practisem3/m21/tictactoe.py
+++ cspp1-practisem3/m21/tictactoe.py
@@ -3,16 +3,13 @@ def Tic_Tac_Toe(matrix):
 	for row in matrix:
 		if row[0] == row[1] == row[2]:
 			winner.append(row[0])
-	print(winner)
 	for i in range(0,3):
 		if matrix[0][i] == matrix[1][i] == matrix[2][i]:
 			winner.append(matrix[0][i])
 	if matrix[0][0] == matrix[1][1] == matrix[2][2]:
 		winner.append(matrix[0][0])
-	print(winner)
 	if matrix[2][0] == matrix[1][1] == matrix[0][2]:
 		winner.append(matrix[0][2])
-	print(winner)
 	if len(winner) == 0:
 		print('draw')
 		return None
@@ -30,7 +27,6 @@ def main():
 	for _ in range(0, 3):
 		col = input().split(' ')
 		matrix.append(col)
-	print(matrix)
 	Tic_Tac_Toe(matrix)
 if __name__ == '__main__':
    main()
