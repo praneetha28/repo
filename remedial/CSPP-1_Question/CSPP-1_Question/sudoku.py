139931d0aef94a09393ec79760d456d25e97d11c
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-12-21 11:49:47 +0530

    "cspp1_m10_a1_1: 2/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 6/10 "

--- remedial/CSPP-1_Question/CSPP-1_Question/sudoku.py
+++ remedial/CSPP-1_Question/CSPP-1_Question/sudoku.py
@@ -21,7 +21,47 @@ def possibilities(mat):
                         res += each
                 print(res)
 
+def check_sudoku(sudoku):
+    if len(sudoku) == 9:
+        row = 0
+        for i in range(9):
+            if len(sudoku[i]) == 9:
+                row += 1
+        if row == 9:
+            if checkrow(sudoku):
+                if checkcol(sudoku):
+                    return True
+                return False
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
 
+def checkrow(sudoku):
+    for i in range(9):
+        rowlist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for j in range(9):
+            rowlist[int(sudoku[i][j])] += 1
+        temprow = rowlist[1:10]
+        for q in range(9):
+            if temprow[q] == 1 or temprow[q] == 0:
+                continue
+            return False
+    return True
+
+def checkcol(sudoku):
+    for num in range(9):
+        rowlist1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for i in range(9):
+            rowlist1[int(sudoku[i][num])] += 1
+        temprow1 = rowlist1[1:10]
+        for q in range(9):
+            if temprow1[q] == 1 or temprow1[q] == 0:
+                continue
+            return False
+    return True
 def main():
     input1 = input()
     grid=[['0' for x in range(9)]for y in range(9)]
@@ -38,7 +78,10 @@ def main():
                     count += 1
                 k += 1
     if count == 9:
-        checkSudoku(grid)
+        if (checkSudoku(grid)):
+            print("Given sudoku is solved")
+        else:
+            print("Invalid Sudoku:Duplicate values")
     else:
         possibilities(grid)
 
